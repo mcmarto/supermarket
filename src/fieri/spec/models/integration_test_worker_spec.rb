acf8cbc132c9209d9f56756f6907d70901448a30
Nell Shamrell <nellshamrell@gmail.com>
2017-03-14 13:59:22 -0700

    wip
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- src/fieri/spec/models/integration_test_worker_spec.rb
+++ src/fieri/spec/models/integration_test_worker_spec.rb
@@ -2,5 +2,10 @@ require 'rails_helper'
 
 describe IntegrationTestsWorker do
   let(:itw) { IntegrationTestsWorker.new }
+  let(:cookbook_json_response) { File.read('spec/support/cookbook_source_url_fixture.json') }
+  let(:cookbook_name) { 'apache' }
 
+  context 'when a source url is present' do
+    let(:octokit) { Octokit::Client.new(access_token: ENV['FIERI_SUPERMARKET_ENDPOINT']) }
+  end
 end
